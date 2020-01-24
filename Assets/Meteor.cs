4939b1f81ed6782e7c15867899db9bdc2c287dfc
hamza herbou <35342314+herbou@users.noreply.github.com>
2020-01-24 15:13:10 +0100

    Add files via upload

---
+++ Assets/Meteor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+	Transform meteorHolder;
+	Animator meteorAnimator;
+
+	void Start ()
+	{
+		meteorHolder = transform.parent;
+		meteorAnimator = GetComponent <Animator> ();
+	}
+
+	public void RandomizeMeteor ()
+	{
+		float angle = Random.Range (0f, 360f);
+		meteorHolder.RotateAround (meteorHolder.position, Vector3.forward, angle);
+
+		float scale = Random.Range (0.8f, 1.4f);
+		meteorHolder.localScale = new Vector3 (scale, scale, scale);
+
+		float speed = Random.Range (0.1f, 1f);
+		meteorAnimator.speed = speed;
+	}
+
+}
